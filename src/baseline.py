01ed6cf0eecba76eacefcf342fbd84465b1c9a4b
anyuanay <anyuanay@gmail.com>
2022-11-23 16:20:33 -0500

    add baseline.py

---
+++ src/baseline.py
@@ -0,0 +1,20 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s -  %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+class PromptSet(Dataset):
+    def __init__(self, prompts):
+        self.prompts = prompts
+
+    def __len__(self):
+        return len(self.prompts)
+
+    def __getitem__(self, index) -> T_co:
+        return self.prompts[index]
+
